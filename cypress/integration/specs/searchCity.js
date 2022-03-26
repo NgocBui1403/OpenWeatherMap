f52c00500f6c42b2bb0dd69e98f8bdf0c47295db
Ngoc Bui <ngoc.buithikim@gmail.com>
2022-03-26 22:38:35 +0700

    init project

---
+++ cypress/integration/specs/searchCity.js
@@ -0,0 +1,18 @@
+describe('Verify searching for a city', () => {
+
+   //TC01-Verify the current date of the searched city
+   //Visit: https://openweathermap.org/
+   //Click the search textbox
+   //Type the city's name
+   //Click on the link in the result list
+   //Verify the current date, city name and the weather display correct
+   
+    it('Verify the current date', () => {
+
+        
+
+    })
+
+    
+
+})
